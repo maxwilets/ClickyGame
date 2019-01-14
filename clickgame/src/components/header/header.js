fb35da6d16d2f581e4071c81bee424c4cf5d0d60
Max Wilets <mwilets@gmail.com>
2019-01-14 15:46:36 -0700

    1st step

---
+++ clickgame/src/components/header/header.js
@@ -0,0 +1,13 @@
+//component from 
+import React from "react"
+
+const Nav = ()=>{
+    return(
+    <header className="nav">
+      <h1>Welcome to Pokemon Click Game!</h1>
+      <h3> Click on a Pokeomon but not the Same on Twice!</h3>
+    </header>
+    )
+}
+
+export default Nav
