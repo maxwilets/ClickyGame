fb8b2da6a2d1e13e1b6b4499b8a256f29526c934
Max Wilets <mwilets@gmail.com>
2019-01-17 11:50:58 -0700

    finished

--- clickgame/src/components/board/Score.js
+++ clickgame/src/components/board/Score.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-const Score = props=> (<div className="score">Last Guess: {props.guess} Score: {props.score}</div>)
+const Score = props=> (<div className="score"><h5>Last Guess: {props.guess} Score: {props.score}</h5></div>)
 
 export default Score
